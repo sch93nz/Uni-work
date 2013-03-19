8e139c011e5d89dbddbf48fa68d0a240adb3efb2
sch93nz <sch93nz@hotmail.com>
2013-03-19 22:03:20 +1300

    so its the busTiemTable project is up here

--- BusTimeTable/src/com/UniWork/bustimetable/Downloading.java
+++ BusTimeTable/src/com/UniWork/bustimetable/Downloading.java
@@ -1,19 +1,48 @@
 package com.UniWork.bustimetable;
 
+import java.io.FileNotFoundException;
+
 import systemManagment.XmlReader;
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
 
 public class Downloading extends Activity {
 
+	
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_downloading);
 		XmlReader getter = new XmlReader();
+		try {
+			
+			String[] settings =getter.getInfo(this.openFileInput("settings.dat"));
+			if (settings[1].equalsIgnoreCase("t")){// has data
+				next();
+			}
+			
+			if (settings[0].equalsIgnoreCase("t")){ // auto update
+				///check version of xml
+			}
+			
+		} catch (FileNotFoundException e) {
+			try {
+				getter.createSettings(this.openFileOutput("settings.dat", Context.MODE_PRIVATE));
+			} catch (FileNotFoundException e1) {
+				e1.printStackTrace();
+			}
+			e.printStackTrace();
+		}
 		
 		
+		
+	}
+
+	private void next() {
 		
 		
 	}
--- BusTimeTable/src/systemManagment/XmlReader.java
+++ BusTimeTable/src/systemManagment/XmlReader.java
@@ -4,6 +4,7 @@
 package systemManagment;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 /**
@@ -14,16 +15,36 @@ public class XmlReader {
 /**
  *  reads in the starting info so when was last update and what the version was also the user options
  * @param fis
+ * @return 
  */
-	public void getInfo(FileInputStream fis){
+	public String[] getInfo(FileInputStream fis){
 		
 		try {
 			byte[] data = new byte[fis.available()+20];
+			 fis.read(data);
+			 String info = new String(data);
+			 String autoUpdate = info.substring(0, 1);
+			 String hasData = info.substring(1,info.indexOf(':'));
+			 String version = info.substring(info.indexOf(':'));
+			 
+			 return new String[]{autoUpdate, hasData, version};
 			 
 		} catch (IOException e) {
 
 		}
+		return new String[]{"T","F","000"};
 
 	}
+
+public void createSettings(FileOutputStream FoS) {
+	try {
+		FoS.write("TF:0000".getBytes());
+		FoS.close();
+	} catch (IOException e) {
+		
+		e.printStackTrace();
+	}
+	
+}
 
 }
