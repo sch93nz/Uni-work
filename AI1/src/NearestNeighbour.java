621ad69c9f28f5bd2d2081032c83bada73374d71
sch93nz <sch93nz@hotmail.com>
2013-03-23 00:01:40 +1300

    so just soem more work were i add in soem code

--- AI1/src/NearestNeighbour.java
+++ AI1/src/NearestNeighbour.java
@@ -1,5 +1,10 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * 
@@ -18,15 +23,68 @@ public class NearestNeighbour {
 	 */
 	private static boolean verbose = false;
 	private static boolean debug = false;
+	private double Range;
+	private List<Nodes> fileInfo;
+	private List<Nodes> trainingInfo;
 
 
+	public NearestNeighbour(List<String> commands) {
+		start(commands.get(0),commands.get(1));
+	}
+
+	public NearestNeighbour(String[] args) {
+		start(args[0],args[1]);
+	}
+
+	private void start(String file, String training) {
+		try {
+			fileInfo = loadData(file);
+			trainingInfo = loadData(training);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 	
-	
+	/**
+	 * opens the file reads it all in and saves the data as a list of Nodes were 
+	 * Each item has its own node which contains the factors it needs..
+	 * @param s
+	 * @return
+	 * @throws IOException
+	 */
+	private List<Nodes> loadData(String s) throws IOException {
+		
+		
+		File file = new File(s);
+		if (!file.exists()){
+			throw new IOException();
+		}
+		
+		
+		BufferedReader buff = new BufferedReader(new FileReader(file)); // buffering it as i know this is going to be bad
+		Scanner scan = new Scanner(buff);// made it a scanner so i can read in one item at a time
+		
+		List<Nodes> list = new ArrayList<Nodes>();
+		while(scan.hasNextLine()){
+			
+			
+			
+			
+			
+			
+		}
+		
+		return list;
+	}
 
 	public static void main(String[] args) {
 		if (args.length <2 || args.length >2){
 			if (args.length== 0){
 				System.out.println("Please give two files for the paramenters -? for help..");
+				System.out.println("Were the first one is the file that contains the data and\n" +
+						"and the second one contans the training set..");
 			}
 			else {
 				List<String> commands = new ArrayList<String>();
@@ -42,15 +100,11 @@ public class NearestNeighbour {
 						commands.add(s);
 					}
 				}
-				
+				new NearestNeighbour(commands);
 			}
-		
 		}
-		
-		
+		new NearestNeighbour(args);
 	}
-
-
 	/**
 	 * this is prints out the help instructions of the file
 	 */
@@ -69,12 +123,19 @@ public class NearestNeighbour {
 		buf.add(" -d\t=\tThis is the debug command i will let you have one guess\n" +
 				"   \t \twhat it does. Thats right it runs the program in dubg mode not\n" +
 				"   \t \tto be confused with verbose as they are different!!\n");
-		
-		
+
+
 		for (String s : buf){
 			System.out.println(s);
 		}
 
 	}
+
+	private class Nodes{
+
+
+	}
+
+
 
 }
