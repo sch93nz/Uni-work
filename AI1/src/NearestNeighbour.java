4c30d0cb49b8f2a134fed818b35a0135abbb0923
sch93nz <sch93nz@hotmail.com>
2013-03-24 23:37:40 +1300

    hope this going correctly

--- AI1/src/NearestNeighbour.java
+++ AI1/src/NearestNeighbour.java
@@ -5,10 +5,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
-/**
- * 
- */
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author schmidmatt
@@ -16,9 +13,7 @@ import java.util.Scanner;
  */
 public class NearestNeighbour {
 
-
 	/**
-	 * d = math.sqr(sum(a1 - b1)^2/R^2)
 	 * @param args
 	 */
 	private static boolean verbose = false;
@@ -26,8 +21,8 @@ public class NearestNeighbour {
 	private double Range;
 	private List<Nodes> fileInfo;
 	private List<Nodes> trainingInfo;
-
-
+	private List<Results> results;
+	private long start;
 	public NearestNeighbour(List<String> commands) {
 		start(commands.get(0),commands.get(1));
 	}
@@ -36,17 +31,135 @@ public class NearestNeighbour {
 		start(args[0],args[1]);
 	}
 
-	private void start(String file, String training) {
+	private void start(String training, String file) {
+		start = System.nanoTime();
 		try {
+
+			if(verbose)System.out.println("Loading the data in to memory");
 			fileInfo = loadData(file);
+			if(verbose)System.out.println("Finished loading the data in to memory");
+			if(debug)System.out.println("loaded data size: "+fileInfo.size());
+
+
+			if(verbose)System.out.println("Loading the training info in to memory");
 			trainingInfo = loadData(training);
+			if(verbose)System.out.println("Finished loading the training info in to memory");
+			if(debug)System.out.println(fileInfo.size());
+
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
+		for( int i=0; i<fileInfo.get(0).getNumValues();i++){
+			Range = getRange(i);
+			Compare(i);
+		}
+		printResult();
+		printTime(System.nanoTime());
+
 	}
 
-	
+	private void printTime(long E) {
+		long time = E-start;
+		long millis = time / 1000000l;
+		
+		String formTime = String.format("%02d:%02d:%02d.%02d", 
+				TimeUnit.MILLISECONDS.toHours(millis),
+				TimeUnit.MILLISECONDS.toMinutes(millis) -  
+				TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millis)), // The change is in this line
+				TimeUnit.MILLISECONDS.toSeconds(millis) - 
+				TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis)),
+				TimeUnit.MILLISECONDS.toMillis(millis));   
+		
+		
+		
+		System.out.println("The operation took: "+formTime+" HH:MM:SS.fff");
+	}
+
+	private void printResult() {
+		if (verbose) System.out.println("printing the results");
+		for(Results r: results){
+			System.out.println(r.toString());
+		}
+
+	}
+
+	/**
+	 * d = math.sqr(sum(a1 - b1)^2/R^2)
+	 * @param i
+	 */
+	private void Compare(int i) {
+
+		if (results== null)	results = new ArrayList<Results>();
+
+		for(Nodes n: fileInfo){
+			if (debug) System.out.println(n.toString());
+			if (!contain(n.getName())){	results.add(new Results(n.getName(),n.getNumValues()));	}
+
+			for (Nodes t: trainingInfo){
+
+				if (n.getName().equalsIgnoreCase(t.getName())){
+
+					double [] temp = getResult(n.getName()).getValues();
+
+					temp[i] = temp[i] + (Math.pow(n.getValue(i)-t.getValue(i),2)/Math.pow(Range,2));
+
+					getResult(n.getName()).setValues(temp);
+				}
+			}
+		}
+
+		if (verbose) {
+			System.out.println("finished getting the sum");
+			System.out.println("now square rooting the sum");
+		}
+
+		SquareResults(i);
+
+		if (verbose) System.out.println("finished squaring the results");
+
+	}
+
+	private void SquareResults(int i) {
+		for (Results r: results){
+			r.setExactResult(i, Math.sqrt(r.getExactResult(i)));
+		}
+
+	}
+
+	private Results getResult(String name) {
+		for (Results n:results){
+			if (n.getName().equalsIgnoreCase(name))return n;
+		}
+		return null;
+	}
+
+	private boolean contain(String name) {
+		for(Results n: results){
+			if (n.getName().equalsIgnoreCase(name)){
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private double getRange(int index) {
+		double max=Double.MIN_VALUE;
+		double min=Double.MAX_VALUE;
+
+		for (Nodes n: fileInfo){
+			if(max <n.getValue(index))max = n.getValue(index);
+		}
+		if (debug) System.out.println(" Max is :" +max);
+
+		for (Nodes n: trainingInfo){
+			if(min >n.getValue(index))min = n.getValue(index);
+		}
+		if (debug) System.out.println(" Min is :" +min);
+
+		return Math.abs(min - max);
+	}
+
 	/**
 	 * opens the file reads it all in and saves the data as a list of Nodes were 
 	 * Each item has its own node which contains the factors it needs..
@@ -54,28 +167,51 @@ public class NearestNeighbour {
 	 * @return
 	 * @throws IOException
 	 */
+	@SuppressWarnings("resource")
 	private List<Nodes> loadData(String s) throws IOException {
-		
-		
+
+		//need to add the debug statements
+		//need to add the verbose statements
+		if (debug||verbose) System.out.println("loading file "+ s);
+
 		File file = new File(s);
-		if (!file.exists()){
-			throw new IOException();
-		}
-		
-		
+
+		if (!file.exists()){	throw new IOException();	}
+
 		BufferedReader buff = new BufferedReader(new FileReader(file)); // buffering it as i know this is going to be bad
 		Scanner scan = new Scanner(buff);// made it a scanner so i can read in one item at a time
-		
+
 		List<Nodes> list = new ArrayList<Nodes>();
+
 		while(scan.hasNextLine()){
-			
-			
-			
-			
-			
-			
+
+			Nodes node;
+
+			if(debug) System.out.println(list.size());
+
+			double one = -1d;
+			double two = -1d;
+			double three = -1d;
+			double four = -1d;
+
+			if(scan.hasNextDouble()) one = scan.nextDouble();
+			if(scan.hasNextDouble()) two = scan.nextDouble();
+			if(scan.hasNextDouble()) three = scan.nextDouble();
+			if(scan.hasNextDouble()) four = scan.nextDouble();
+			String name =scan.nextLine();
+
+			if (debug) System.out.println(" "+one+", "+two+", "+three+", "+four+", "+name);
+			if (one >=0d && two >=0d && three >= 0d && four >= 0d & name.length()>0){
+				node = new Nodes(one,two,three,four,name);
+
+				if (debug) System.out.println(node.toString());
+
+				list.add(node);
+			}
 		}
-		
+
+		if (verbose)System.out.println("A total of " + list.size()+ " lines of data was loaded");
+
 		return list;
 	}
 
@@ -102,9 +238,11 @@ public class NearestNeighbour {
 				}
 				new NearestNeighbour(commands);
 			}
+		}else {
+			new NearestNeighbour(args);
 		}
-		new NearestNeighbour(args);
 	}
+
 	/**
 	 * this is prints out the help instructions of the file
 	 */
@@ -132,10 +270,104 @@ public class NearestNeighbour {
 	}
 
 	private class Nodes{
+		private List<Double> values;
+		private String name;
 
+		/**
+		 * @param sepalLength
+		 * @param sepalWidth
+		 * @param petalLength
+		 * @param petalWidth
+		 * @param name
+		 */
+		public Nodes(double sepalLength, double sepalWidth, double petalLength,
+				double petalWidth, String name) {
+			super();
+			values = new ArrayList<Double>();
+			values.add(sepalLength);
+			values.add(sepalWidth);
+			values.add(petalLength);
+			values.add(petalWidth);
+			this.name = name;
+		}
+
+		public double getValue(int index){
+			return values.get(index);
+		}
+
+		public int getNumValues(){
+			return values.size();
+		}
+
+		/**
+		 * @return the name
+		 */
+		public String getName() {
+			return name;
+		}
+		public String toString(){
+			StringBuffer buff = new StringBuffer();
+			buff.append("|");
+			for (double d : values){
+				buff.append(d+" | ");
+			}
+			buff.append(name);
+			return buff.toString();
+		}
 
 	}
 
+	private class Results{
+		private String name;
+		private double[] values;
 
+		public Results(String n, int numOfValues){
+			name = n;
+			values = new double[numOfValues];	
+			for (int i = 0; i < numOfValues ; i++){
+				values[i] = 0d;
+			}
+		}
 
+		/**
+		 * @return the values
+		 */
+		public double[] getValues() {
+			return values;
+		}
+
+		/**
+		 * @param values the values to set
+		 */
+		public void setValues(double[] values) {
+			this.values = values;
+		}
+
+		public void setExactResult(int index, double value){
+			values[index]= value;
+		}
+
+		public double getExactResult(int index){
+			return values[index];
+		}
+
+		/**
+		 * @return the name
+		 */
+		public String getName() {
+			return name;
+		}
+
+		public String toString(){
+			StringBuffer buff = new StringBuffer();
+			buff.append("| ");
+			for (double d : values){
+				buff.append(d+"\t|\t");
+			}
+			buff.append(name);
+
+			return buff.toString();
+		}
+
+	}
 }
